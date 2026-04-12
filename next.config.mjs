9a0bd889418915948f8df589687505f7387e5bd8
farhan-ramzan1 <farhanramzaan1@gmail.com>
2026-04-12 14:59:34 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://namiru.ai https://evident-gibbon-87.clerk.accounts.dev;
-              connect-src 'self' https://namiru.ai https://evident-gibbon-87.clerk.accounts.dev;
-              img-src 'self' data: https://img.clerk.com https://namiru.ai;
-              style-src 'self' 'unsafe-inline';
-              frame-src 'self' https://namiru.ai;
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
-  },
   experimental: {
-    serverComponentsHmrCache: false,
+    serverComponentsHmrCache: false, // defaults to true
   },
   images: {
     remotePatterns: [
