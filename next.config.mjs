9b28043975257e20dcbe8f1f43e3427b58b928fc
farhan-ramzan1 <farhanramzaan1@gmail.com>
2026-04-22 02:10:57 +0500

    Update next.config.mjs latesttttt

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,16 @@
-/** @type {import('next').NextConfig} */ const nextConfig = { experimental: { serverComponentsHmrCache: false, // defaults to true }, images: { remotePatterns: [ { protocol: "https", hostname: "img.clerk.com", }, ], }, }; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
